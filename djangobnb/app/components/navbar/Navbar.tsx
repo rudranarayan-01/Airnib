6ef621bf50ebbcfac25dda2032aa32637efaffbe
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-08-05 18:53:56 +0530

    nav

--- djangobnb/app/components/navbar/Navbar.tsx
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import SearchFilters from './SearchFilters'
 
 const Navbar = () => {
     return (
-        <nav className='w-full fixed top-0 left-0 py-6 border-b text-orange-500 bg-white z-10'>
+        <nav className='w-full fixed top-0 left-0 py-6 border-b bg-white text-black z-10'>
             <div className='max-w-[1500px] mx-auto px-6'>
                 <div className='flex justify-between items-center'>
                     <Link href="/" className=''>
                         <Image src={"/logo.png"} alt="Logo" width={180} height={40} className='inline-block mr-2' />
                     </Link>
                     <div className='flex space-x-6 '>
-                        Search filter
+                        <SearchFilters/>
                     </div>
                     <div className='flex space-x-6 items-center'>
                         Add properties - User nav
---
+++ djangobnb/app/components/navbar/SearchFilters.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchFilters = () => {
+    return (
+        <div className='h-[64px] flex flex-row items-center justify-between border rounded-full'>
+            <div className='flex flex-row items-center justify-between'>
+                <div className="w-[250px] h-[64px] px-8 flex flex-col rounded-full hover:bg-gray-100 justify-center">
+                    Hello
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchFilters
